a3449377300dd3ea08ed1c98e20c7eb4a512fc59
Pedro Freitas <pedro.freitas@dbccompany.com.br>
2023-03-29 23:08:01 -0300

    teste

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = new koa()
 const server = http.createServer(app.callback())
 const io = socket(server)
 
-const SERVER_HOST = 'localhost'
+const SERVER_HOST = '0.0.0.0'
 const SERVER_PORT = 8080
 
 const passwords = {
